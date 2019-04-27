f9feefe2c18ed0154311cf6fb0fc45bdeb4950b1
Jordan Rabinowitz <jordanmarcr@gmail.com>
2019-04-27 14:00:44 -0400

    master bitches

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import '../App.css'
+import palmtree from '../images/palmtree.png'
 
 const Navbar = () => {
   return(
-      <div>
-        <h1> I am here</h1>
+    <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">
+        <img
+            src={palmtree}
+            alt='atl'
+            className='palmtree'
+        />
+        </a>
       </div>
-
+    </div>
+  </nav>
   )
-
 }
 
 export default Navbar
