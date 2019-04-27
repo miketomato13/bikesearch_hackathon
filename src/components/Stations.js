1a283686f9147f324d1967ce2b978f3fb997708e
Eduardo Iglesias <eriglesias@ufl.edu>
2019-04-27 12:07:03 -0400

    sorted stations array by how far they are from current location. current location still doesn't exist so that must be changed later

--- src/components/Stations.js
+++ src/components/Stations.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
 const Stations = (props) => {
+  let stations = props.stations
+  let currentLat = 25.7760782;
+  let currentLong = -80.194204;
+  stations = stations.sort((a,b) => {
+    let distA = props.distance(a.latitude, a.longitude, currentLat, currentLong)
+    let distB = props.distance(b.latitude, b.longitude, currentLat, currentLong)
+    if(distA < distB)
+    {
+      return -1
+    }
+    else if(distA > distB)
+    {
+      return 1;
+    }
+    return 0;
+
+  })
   return (
     <div>
       <table>
@@ -13,12 +30,12 @@ const Stations = (props) => {
         </thead>
         <tbody>
           {
-          props.stations.map(station =>{
+            stations.map(station => {
 
             return(
             <tr>
               <td>{station.extra.address}</td>
-              <td>{props.distance(station.latitude, station.longitude, 25.7760782,  -80.194204, 'M').toFixed(2)} miles</td>
+              <td>{props.distance(station.latitude, station.longitude, currentLat,  currentLong, 'M').toFixed(2)} miles</td>
               <td>{station.free_bikes}</td>
             </tr>
               )
