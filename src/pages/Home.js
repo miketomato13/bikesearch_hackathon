ab7eac8bc667ca6280c194fc52f7c5f5a68f41f6
Jordan Rabinowitz <jordanmarcr@gmail.com>
2019-04-27 09:44:35 -0400

    added api state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Navbar from '../components/Navbar'
+import axios from 'axios'
 
 
-const Home = () => {
-  return(
-    <div>
+export default class Home extends Component {
+  state = { data: []
+          }
+
+  componentDidMount() {
+    axios.get(`https://api.citybik.es/v2/networks/decobike-miami-beach`)
+      .then(res => {
+        this.setState({ data: res.data })
+        console.log(this.state.data)
+      })
+    }
+
+  render() {
+    return (
+      <div>
         <Navbar/>
-
-
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
-export default Home
