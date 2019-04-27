727f5641bc6ddee037f7b6b57abfb536dd627285
MikeTomato <mike@costabellarealtygroup.com>
2019-04-27 09:03:12 -0400

    committing navbar and home stuff

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+
+const Home = () => {
+  return(
+    <div>
+        <NavBar/>
+      
+
+    </div>
+  )
+}
+
+export default Home
